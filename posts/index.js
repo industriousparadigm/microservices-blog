8edeea61f34d2d59d644bd7f8b23fafea79dc0d6
industriousparadigm <dsgmcosta@gmail.com>
2020-04-23 14:24:43 +0100

    posts service created

---
+++ posts/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const { randomBytes } = require('crypto')
+// const cors = require('cors')
+// const axios = require('redaxios')
+
+const PORT = 4000
+
+const app = express()
+app.use(express.json())
+
+const posts = {}
+
+app.get('/posts', (req, res) => {
+  res.send(posts)
+})
+
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex')
+  const { title } = req.body
+
+  posts[id] = {
+    id,
+    title,
+  }
+
+  res.status(201).send(posts[id])
+})
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`)
+})
